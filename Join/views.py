a58dd5bc4e17ce5f0d4c77407503461faab837c1
niccokuc <nerminkuc74@gmail.com>
2016-01-26 17:26:03 +0100

    0.0.2 - Signup.html + Jions

--- Join/views.py
+++ Join/views.py
@@ -3,14 +3,16 @@ from .forms import JoinForm
 from .models import Join
 
 # how the page is viewed
-def home(request):
+def JoinView(request):
     form = JoinForm(request.POST or None)
-    if form.is_valid():
-        new_join = form.save(commit=False)
-        email = form.cleaned_data['email']
-        new_join_old, created = Join.objects.get_or_create(email=email)
 
-    context = {"form": form}
-    print(context)
-    template = 'signup.html'
-    return render(request, template, context)
+    if form.is_valid():
+        print("THANKS")
+        new_join = form.save(commit=False)
+        email = form.cleaned_data['varEmail']
+        name = form.cleaned_data['varName']
+        user = form.cleaned_data['varUser']
+        print(email)
+        new_join_old, created = Join.objects.get_or_create(varEmail=email, varName=name, varUser=user)
+    return render(request, 'signup.html', {'form': form})
+
